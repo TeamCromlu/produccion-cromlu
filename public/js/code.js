aeb8e8d178550ab930cc3c6971c5e306434e3c01
Joel Gonzales Tipismana <joelengt@gmail.com>
2015-11-12 13:38:26 -0500

    nuevo camino de planes

--- public/js/code.js
+++ public/js/code.js
@@ -1,5 +1,6 @@
 $(document).ready(main);
-$(document).ready(adios);
+$(document).ready(adiosMenu);
+// $(document).ready(validarFormulario);
 
 
 var contador = 1;
@@ -22,7 +23,7 @@ function main(){
 };
 
 
-function adios(){
+function adiosMenu(){
 	$('.Header__botones').click(function (){
 		if (contador==0) {
 			contador = 0;
@@ -33,3 +34,16 @@ function adios(){
 		}
 	});
 };
+
+
+
+// function validarFormulario(){
+//   var nombre = document.forms["form1"]["nombre"].value;
+//   var email = document.forms["form1"]["email"].value;
+  
+//   if(nombre == "" && email == ""){
+//     document.getElementById("name").style.border="1px solid red";
+//     document.getElementById("mail").style.border="1px solid red";
+//     error = true;
+//   }
+// }
